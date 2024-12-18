392d948847ad54207c456cfc6c5f919ba580663e
nhattruong022 <nhattruongp78@gmail.com>
2024-12-18 14:44:21 +0700

    update productmodel and categorymodel

--- Shop Classix/Repository/DataContext.cs
+++ Shop Classix/Repository/DataContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Shop_Classix.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Shop_Classix.Models;
+
+namespace Shop_Classix.Repository
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<ProductsModel> products { get; set; }
+        public DbSet<CategoryModel> categories { get; set; }
     }
 }
