7d4babd723db0131fc85edce7ee942414de85a1d
Le Van Vui <vvanvui42@gmail.com>
2024-12-30 13:12:13 +0700

    feat: Thêm api Product trong areas

--- Shop Classix/obj/Debug/net8.0/Shop Classix.AssemblyInfo.cs
+++ Shop Classix/obj/Debug/net8.0/Shop Classix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop Classix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5189a6dcb5b983667000371757c8217798a2f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae451961d95bca61b8b3514a83ac5c25291df00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop Classix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop Classix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
