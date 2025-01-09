1664cdf5712794ad78d44dfc3578407698bcdb53
kienxuan1458 <kienxuan1458@gmail.com>
2025-01-09 15:07:36 +0700

    cập nhật code

--- ShopClassix/Models/ViewModels/CommentViewModel.cs
+++ ShopClassix/Models/ViewModels/CommentViewModel.cs
@@ -2,12 +2,12 @@
 {
     public class CommentViewModel
     {
-        public int CommentId { get; set; } // Comment ID
-        public string CustomerName { get; set; } = string.Empty; // Customer Name
-        public string ProductName { get; set; } = string.Empty; // Product Name
-        public string ProductImage { get; set; } = string.Empty; // Product Image
-        public string Category { get; set; } = string.Empty; // Category
-        public string Content { get; set; } = string.Empty; // Comment Content
-        public int Rating { get; set; } // Rating (1 to 5)
+        public int CommentId { get; set; } 
+        public string CustomerName { get; set; } = string.Empty; 
+        public string ProductName { get; set; } = string.Empty; 
+        public string ProductImage { get; set; } = string.Empty; 
+        public string Category { get; set; } = string.Empty; 
+        public string Content { get; set; } = string.Empty; 
+        public int Rating { get; set; } 
     }
 }
