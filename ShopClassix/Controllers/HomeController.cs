fe8e2dee6e0c80ae31dd69910f0bf7b3c5ab5e86
Luong Anh Duy <luonganhduy1002@gmail.com>
2025-01-10 21:15:54 +0700

    Update: Cập nhựt lại cách hiển thị Gender của thông tin khách hàng và xét trạng thái hiển thị sản phẩm ở trang chủ

--- ShopClassix/Controllers/HomeController.cs
+++ ShopClassix/Controllers/HomeController.cs
@@ -121,13 +121,9 @@ namespace Shop_Classix.Controllers
                 HttpContext.Session.SetString("Phone", firstContact.PhoneNumber);
                 HttpContext.Session.SetString("Email", firstContact.Email);
                 HttpContext.Session.SetString("Logo", firstContact.Logo);
-<<<<<<< HEAD
             }
 
 
-=======
-            }
->>>>>>> d8e92bee1560337cc4f0903e8563d41a09888e01
             ViewBag.UniqueProductCount = cart.Items.Select(item => item.ProductId).Distinct().Count();
             ViewBag.TotalAmount = cart.TotalAmount;
 
@@ -283,11 +279,11 @@ namespace Shop_Classix.Controllers
             });
         }
 
-       
 
 
 
-  
+
+
 
 
 
