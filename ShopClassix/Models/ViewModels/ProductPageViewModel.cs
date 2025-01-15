e6a70a4ac45287f87eccfdbc6672e3ba4b59cdad
kienxuan1458 <kienxuan1458@gmail.com>
2025-01-15 21:46:53 +0700

    Cập nhập View home controller

--- ShopClassix/Models/ViewModels/ProductPageViewModel.cs
+++ ShopClassix/Models/ViewModels/ProductPageViewModel.cs
@@ -4,5 +4,7 @@
     {
         public IEnumerable<ProductsModel> AllProducts { get; set; } 
         public IEnumerable<ProductsModel> FavoriteProducts{ get; set; }
+        public IEnumerable<ProductsModel> TopRatedProducts { get; set; }
+        public IEnumerable<ProductsModel> NewProducts { get; set; }
     }
 }
